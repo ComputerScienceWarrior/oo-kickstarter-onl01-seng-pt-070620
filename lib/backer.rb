c5cc16dce37fa8355fe4ee8cd55f1efe0e696915
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-29 16:03:28 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,7 +11,7 @@ class Backer
   end
   
   def back_project(project)
-    binding.pry
+    # binding.pry
     @backed_projects << project 
     Project.all.find {|proj| proj.title == self.title}
   end
